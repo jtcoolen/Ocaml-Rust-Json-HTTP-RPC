a1cba31ef09d8cd03645898e6502f66143e61de3
Julien Coolen <jtcoolen@pm.me>
2024-03-29 15:47:15 +0100

    streaming works for ocaml server<->ocaml client

--- rust/bin_client/src/main.rs
+++ rust/bin_client/src/main.rs
@@ -1,6 +1,8 @@
 #![deny(warnings)]
 #![warn(rust_2018_idioms)]
 
+//use std::io::Read;
+
 use bytes::{Buf, Bytes};
 use http_body_util::{BodyExt, Empty};
 use hyper::Request;
@@ -14,7 +16,7 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>
 async fn main() -> Result<()> {
     let url = "localhost".parse().unwrap();
     let version = fetch_json(url).await?;
-    // print users
+    // print version
     println!("version: {:#?}", version);
 
     Ok(())
@@ -50,7 +52,61 @@ async fn fetch_json(url: hyper::Uri) -> Result<String> {
     let body = res.collect().await?.aggregate();
 
     // try to parse as json with serde_json
-    let users = serde_json::from_reader(body.reader())?;
+    let version = serde_json::from_reader(body.reader())?;
+    println!("version: {:#?}", version);
 
-    Ok(users)
+    // Fetch the url...
+    let req = Request::builder()
+        .method("GET")
+        .uri("/stream")
+        .header(hyper::header::HOST, authority.as_str())
+        .body(Empty::<Bytes>::new())?;
+
+    let res: hyper::Response<hyper::body::Incoming> = sender.send_request(req).await?;
+
+    // asynchronously aggregate the chunks of the body
+    let body = res.collect().await?.aggregate();
+
+    // Create a vector to hold the contents read from the buffer
+    /*let mut contents = Vec::new();
+
+    let mut reader = body.reader();
+    // Read contents from the buffer in a loop until the end
+    loop {
+        let mut chunk = [0; 8]; // Define a chunk size
+        let bytes_read = reader.read(&mut chunk)?;
+
+        if bytes_read == 0 {
+            break; // If no bytes were read, we've reached the end of the buffer
+        }
+
+        // Append the read bytes to the contents vector
+        contents.extend_from_slice(&chunk[..bytes_read]);
+    }
+
+    println!(
+        "data: {:?}",
+        std::str::from_utf8(&contents)?
+            .lines()
+            .map(String::from)
+            .map(|s| serde_json::from_str(&s))
+            .collect::<Vec<std::result::Result<String, serde_json::Error>>>()
+    );
+
+    */
+
+    let deserializer = serde_json::Deserializer::from_reader(body.reader());
+    let iterator = deserializer.into_iter::<serde_json::Value>();
+    for item in iterator {
+        println!("Got {:?}", item?);
+    }
+    // try to parse as json with serde_json
+    /*let data: String = serde_json::from_reader(body.reader()).unwrap_or_else(|e| {
+        eprintln!("\nError: {}", e);
+        ::std::process::exit(1);
+    });
+
+    println!("data: {:#?}", data);*/
+
+    Ok(version)
 }
